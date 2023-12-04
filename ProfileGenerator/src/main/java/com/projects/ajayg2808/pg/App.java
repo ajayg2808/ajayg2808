4d5c58d0cd767f3082df908b2497bc5b6a4aaaab
Ajay Gaikwad <34316783+ajayg2808@users.noreply.github.com>
2023-12-04 10:30:28 +0000

    Empty simple maven project

---
+++ ProfileGenerator/src/main/java/com/projects/ajayg2808/pg/App.java
@@ -0,0 +1,13 @@
+package com.projects.ajayg2808.pg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
